074170e831cb9be3d58c37708c217dccb3c10a20
Marcos <marcos.zanca@outlook.com.br>
2023-06-03 15:30:50 -0300

    Criado classe modeloTabela

--- src/modelo/ModeloTabela.java
+++ src/modelo/ModeloTabela.java
@@ -1,23 +1,62 @@
-
 package modelo;
 
+import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
-public class ModeloTabela extends AbstractTableModel{
+public class ModeloTabela extends AbstractTableModel {
+
+    private ArrayList linhas = null;//tipo de um vetor para linhas
+    private String[] colunas = null;//vetor para colunas
+
+    //tentando fazer uso do padro static metodo
+    private ModeloTabela(ArrayList numLimhas, String[] numColunas) {//passa parametros
+        setLinhas(numLimhas);
+        setColunas(numColunas);
+    }
+
+    //para chamar da classe(new moelotabela)
+    //com um nome significativo ver renderTabela
+    public static ModeloTabela gerarTabelaComLinhasEColunas(ArrayList lin, String[] col) {
+        return new ModeloTabela(lin, col);
+    }
 
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return linhas.size();//tamanho do array
     }
 
     @Override
-    public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getColumnCount() {//total colunas
+        return colunas.length;
     }
 
-    @Override
-    public Object getValueAt(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Override//passa parametros para monstar a tabela
+    public Object getValueAt(int numLin, int numCol) {
+        Object[] linha = (Object[]) getLinhas().get(numLin);
+        return linha[numCol];
     }
-    
+
+    //metodos  digitados
+    @Override
+    public String getColumnName(int numCol) {
+        return colunas[numCol];
+    }
+
+    //campos encapsulados..
+    public ArrayList getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(ArrayList linhas) {
+        this.linhas = linhas;
+    }
+
+    public String[] getColunas() {
+        return colunas;
+    }
+
+    public void setColunas(String[] colunas) {
+        this.colunas = colunas;
+    }
+
 }
